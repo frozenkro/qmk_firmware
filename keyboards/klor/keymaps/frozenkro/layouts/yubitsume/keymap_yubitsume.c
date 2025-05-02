e3e2cda5c4bf6e0a26029423c053d4bc326cfc13
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-05-01 23:00:12 -0500

    klor yubitsume thumb cluster rework

--- keyboards/klor/keymaps/frozenkro/layouts/yubitsume/keymap_yubitsume.c
+++ keyboards/klor/keymaps/frozenkro/layouts/yubitsume/keymap_yubitsume.c
@@ -25,9 +25,9 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         case HM_A:
             return 200;
         case HM_F:
-            return 100;
+            return 120;
         case HM_J:
-            return 100;
+            return 120;
         case HM_K:
             return 170;
         case HM_D:
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
     HM_A,     HM_S,     HM_D,     HM_F,     KC_G,                          KC_H,     HM_J,     HM_K,     HM_L,     KC_SCLN,
     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,   KC_MPLY,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
-                        KC_LCTL,KC_LALT,LT(1,KC_SPC),LT(2,KC_ESC), LT(2,KC_BSPC),LT(1,KC_ENT),KC_SPC,KC_RSFT
+                        KC_ESC,KC_SPC,MO(1),MO(2), MO(2),LM(1,MOD_RSFT),KC_BSPC,KC_ENT
  ),
 
 
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  KC_7,     KC_8,     KC_9,     KC_EQL,                        _______,  _______,  _______,  _______,  _______,
     KC_TAB,   KC_4,     KC_5,     KC_6,     KC_0,                          KC_LBRC,  KC_9,     KC_0,     KC_RBRC,  _______,
     _______,  KC_1,     KC_2,     KC_3,     KC_MINS,  _______,   _______,  _______,  KC_QUOT,  KC_GRV,   _______,  KC_BSLS,
-                        _______,  _______,  _______,  _______,   KC_DEL,   KC_SPC,   _______,  KC_CAPS
+                        _______,  _______,  _______,  _______,   _______,  KC_DEL,   KC_SPC,   KC_CAPS
  ),
 
    [_ACTIONS] = LAYOUT_yubitsume(
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,                          _______,  _______,  _______,  _______,  _______,
     KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,                          _______,  _______,  _______,  _______,  LT(4,KC_SCLN),
     KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,     _______,   _______,  _______,  _______,  _______,  _______,  _______,
-                        KC_LCTL,  KC_SPC,   _______,  _______,   _______,  _______,  _______,  _______
+                        _______,  KC_LCTL,  KC_SPC,   _______,   _______,  _______,  _______,  _______
  ),
    [_KEYB] = LAYOUT_yubitsume(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷
