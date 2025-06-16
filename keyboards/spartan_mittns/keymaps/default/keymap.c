6151c2a540e1dacbeb4c10ea2371b07b787973fc
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-06-16 18:10:13 -0500

    rework thumb clusters on spartan_mittns

--- keyboards/spartan_mittns/keymaps/default/keymap.c
+++ keyboards/spartan_mittns/keymaps/default/keymap.c
@@ -1,6 +1,7 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <sys/types.h>
 #include QMK_KEYBOARD_H
 
 const uint16_t HM_A = MT(MOD_LGUI,KC_A);
@@ -68,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,     KC_W,    KC_E,   KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_BSPC,
         KC_LSFT, HM_A,     HM_S,    HM_D,   HM_F,    KC_G,                               KC_H,    HM_J,    HM_K,    HM_L,    KC_SCLN, LT(4,KC_QUOT),
         KC_LCTL, KC_Z,     KC_X,    KC_C,   KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,       KC_RCTL,
-                                    KC_LALT, LT(1,KC_SPC), LT(2,KC_ESC),        LT(2,KC_BSPC),  LT(1,KC_ENT),  KC_RSFT
+                                    KC_ESC, LT(1,KC_SPC), MO(2),        LM(2,MOD_LSFT), LT(1,KC_BSPC),  KC_ENT
     ),
     [1] = LAYOUT_split_3x6_3(
         _______, KC_TAB,   KC_7,    KC_8,   KC_9,    KC_EQL,                             _______, _______, _______, _______, _______, _______,
@@ -78,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [2] = LAYOUT_split_3x6_3(
         _______, KC_VOLU,  KC_F7,   KC_F8,  KC_F9,   KC_F10,                             _______, _______, _______, _______, KC_PGUP, _______,
-        _______, KC_VOLD,  KC_F4,   KC_F5,  KC_F6,   KC_F11,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_CAPS,
-        _______, KC_MUTE,  KC_F1,   KC_F2,  KC_F3,   KC_F12,                             KC_PGDN, _______, _______, _______, _______, KC_PSCR,
+        _______, KC_VOLD,  KC_F4,   KC_F5,  KC_F6,   KC_F11,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS, KC_CAPS,
+        _______, KC_MUTE,  KC_F1,   KC_F2,  KC_F3,   KC_F12,                             KC_PGDN, _______, _______, _______, KC_PSCR, KC_PSCR,
                                             _______, _______, _______,          _______, _______, _______
     ),
     /*
